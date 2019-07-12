8d8119d66ef75eabc60a68c17e34c5e6243b735d
EC2 Default User <ec2-user@ip-172-31-95-185.ec2.internal>
2019-07-12 15:48:25 +0000

    alb added

---
+++ alb-test-webserver.tf
@@ -0,0 +1,74 @@
+#Application load balancer for app server
+resource "aws_lb" "front_end" {
+  name               = "${var.PROJECT_NAME}-Front-End-ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.sg-alb-test-webservers.id}"]
+  subnets            = ["${aws_subnet.public_subnet_3.id}", "${aws_subnet.public_subnet_2.id}","${aws_subnet.public_subnet_1.id}"]
+  # set to true for production
+  enable_deletion_protection = false
+
+  depends_on = ["aws_s3_bucket.test_web_alb_logs"]
+
+  # enable for prod
+  access_logs {
+    bucket  = "${aws_s3_bucket.test_web_alb_logs.bucket}"
+    prefix  = "webserver-test-alb-logs"
+    enabled = false
+  }
+
+  tags = {
+    Environment = "Test Env"
+  }
+}
+
+# Add Target Group
+
+resource "aws_lb_target_group" "front_end" {
+  name     = "Target-Group-for-frontend"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.main.id}"
+  health_check {
+                path = "/"
+                port = "80"
+                protocol = "HTTP"
+                healthy_threshold = 2
+                unhealthy_threshold = 10
+                interval = 5
+                timeout = 10
+                matcher = "200-308"
+        }
+}
+
+# Adding HTTP listener
+
+resource "aws_lb_listener" "webserver" {
+  load_balancer_arn = "${aws_lb.front_end.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.front_end.arn}"
+    type             = "forward"
+  }
+}
+
+# for production only https
+
+#resource "aws_lb_listener" "front_end" {
+#  load_balancer_arn = "${aws_lb.front_end.arn}"
+#  port              = "443"
+#  protocol          = "HTTPS"
+#  ssl_policy        = "ELBSecurityPolicy-2016-08"
+#  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+#
+#  default_action {
+#    type             = "forward"
+#    target_group_arn = "${aws_lb_target_group.front_end.arn}"
+#  }
+
+output "WebServer_Load_Balancer_Endpoint" {
+  value = "${aws_lb.front_end.dns_name}"
+}
+
